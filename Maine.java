27d4db81d245812517abe69b440d44bb4aeb1662
Kubwimana Eloge <79303799+elogeKub@users.noreply.github.com>
2023-06-23 21:42:59 -0500

    reading a file (java)

---
+++ Maine.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+class Maine {
+    public static void main(String args[]) {
+
+        try {
+            Scanner fileScanner = new Scanner(new File("file/myFile.txt"));
+
+            while (fileScanner.hasNextLine()) {
+                System.out.println(fileScanner.nextLine());
+
+            }
+            fileScanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
